846f5ea2b02f3dfc734b792f5da6316703fcd5d8
Beerendra M C <beerendramc@gmail.com>
2024-03-26 13:55:01 +0000

    commented os.environ

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-import os
-from apikey import apikey
-
+#import os
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
-os.environ['OPENAI_API_KEY'] = apikey
+#os.environ['OPENAI_API_KEY'] = 'your api key'
 
 # App framework
 st.title('💥 Instagram Caption GPT')
